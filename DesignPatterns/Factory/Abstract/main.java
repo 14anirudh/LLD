aff77114706761ce24c79c1bf2c7c4ae740de374
anirudh gautam <anirudhgautam987@gmail.com>
2025-05-05 00:56:14 +0530

    edit in main

--- DesignPatterns/Factory/Abstract/main.java
+++ DesignPatterns/Factory/Abstract/main.java
@@ -2,7 +2,8 @@ package DesignPatterns.Factory.Abstract;
 
 public class main {
     public static void main(String[] args) {
-        AbstarctFactory factory = new EconomicCarFactory();
+        AbstractFactoryProducer producer = new AbstractFactoryProducer();
+        AbstarctFactory factory = producer.getFactoryInstance("Economic");
         Car car = factory.getInstance(100000);
         System.out.println(car.getTopSpeed()); 
     }
